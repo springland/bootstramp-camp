28cbe6c15fd69e636169b284c0974e191ec9b8ec
springland <fenglinmail@yahoo.com>
2023-04-09 21:54:03 -0400

    Initial commit

---
+++ src/main/java/com/springland365/bootstrampcamp/BootstrampCampApplication.java
@@ -0,0 +1,13 @@
+package com.springland365.bootstrampcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrampCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrampCampApplication.class, args);
+	}
+
+}
